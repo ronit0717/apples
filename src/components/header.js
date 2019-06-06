91d609d789afeb3f10f7d9180aad74c0dc352c9a
Ronit Chattopadhyay <ronit0717@gmail.com>
2019-06-06 09:33:13 +0530

    Dynamic Data

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
+const getYear = () => {
+    const d = new Date();
+    return d.getFullYear();
+}
+
+const user = {
+    name : 'Ronit',
+    surname : 'Chattopadhyay',
+    age : 16
+}
+
 const Header = () => {
-    return <div>Header</div>
+    return (
+        <div>
+            <div> First Name : {user.name}</div>
+            <div> Last Name  : {user.surname}</div>
+            <div> Age        : {user.age}</div>
+
+            <div>
+                Copy Right RC @ {getYear()}
+            </div>
+        </div>
+    )
 }
 
 export default Header;
