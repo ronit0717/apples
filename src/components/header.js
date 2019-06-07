0501975f1ddb972ceff81af6b423b9a1aa343006
RONIT CHATTOPADHYAY <ronit@yourdost.com>
2019-06-07 11:09:34 +0530

    Added style and created header class

--- src/components/header.js
+++ src/components/header.js
@@ -1,28 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const getYear = () => {
-    const d = new Date();
-    return d.getFullYear();
-}
-
-const user = {
-    name : 'Ronit',
-    surname : 'Chattopadhyay',
-    age : 16
-}
-
-const Header = () => {
-    return (
-        <div>
-            <div> First Name : {user.name}</div>
-            <div> Last Name  : {user.surname}</div>
-            <div> Age        : {user.age}</div>
-
-            <div>
-                Copy Right RC @ {getYear()}
-            </div>
-        </div>
-    )
+class Header extends Component {
+   render(){
+       return (
+           <header>
+               <div className = "logo">Logo</div>
+               <input type="text" />
+           </header>
+       )
+   }
 }
 
 export default Header;
