0ca56a7d45382670c5ae845a63c8658fc5e2664f
Ronit Chattopadhyay <ronit0717@gmail.com>
2019-06-09 23:21:04 +0530

    State

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,32 @@
 import React, {Component} from 'react';
 
 class Header extends Component {
+    
+    state = {
+        title: 'The keywords are:',
+        keyword: ''
+    }
+        
+    inputChangeHandler = (event) => { 
+        //console.log(event.target.value);
+        this.setState({
+            keyword: event.target.value
+        })
+    }
 
-   inputChangeHandler(event){ 
-       console.log(event.target.value);
-   }
-
-   render(){
-       return (
-           <header>
-               <div className = "logo">Logo</div>
-               <input 
-                    type="text" 
-                    onChange={ (e) => this.inputChangeHandler(e)}/>
-           </header>
-       )
-   }
+    render(){
+        console.log(this.state.keyword)
+        return (
+            <header>
+                <div className = "logo">Logo</div>
+                <input 
+                        type="text" 
+                        onChange={ this.inputChangeHandler }/>
+                <div> {this.state.title} </div>
+                <div> {this.state.keyword} </div>
+            </header>
+        )
+    }
 }
 
 export default Header;
