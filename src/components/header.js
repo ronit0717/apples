7acb5e0ead6aa295e4f7f8d431e2944e74b6e26c
RONIT CHATTOPADHYAY <ronit@yourdost.com>
2019-06-10 11:49:10 +0530

    Props

--- src/components/header.js
+++ src/components/header.js
@@ -3,19 +3,16 @@ import React, {Component} from 'react';
 class Header extends Component {
     
     state = {
-        title: 'The keywords are:',
         keyword: ''
     }
         
     inputChangeHandler = (event) => { 
-        //console.log(event.target.value);
         this.setState({
             keyword: event.target.value
         })
     }
 
     render(){
-        console.log(this.state.keyword)
         return (
             <header>
                 <div className = "logo">Logo</div>
@@ -23,7 +20,6 @@ class Header extends Component {
                         type="text" 
                         onChange={ this.inputChangeHandler }/>
                 <div> {this.state.title} </div>
-                <div> {this.state.keyword} </div>
             </header>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import JSON from './db.json';
 
 //COMPONENTS
 import Header from './components/header';
+import NewsList from './components/news_list';
 
-const App = () => {
-    return (
-        <div>
-            <Header/>
-        </div>
-    )
+
+class App extends Component {
+    state = {
+        news : JSON
+    }
+
+    render(){
+        return (
+            <div>
+                <Header/>
+                <NewsList news = {this.state.news}/>
+            </div>
+        )
+    }
 }
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
